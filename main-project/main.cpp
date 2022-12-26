6d3d8417b16b90c0dd0a25e037cf849c550d5c9c
ElfikJanchik <121351484+ElfikJanchik@users.noreply.github.com>
2022-12-26 15:27:14 +0300

    Update main.cpp
    Добавлен вывод общей информации о задании

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello\n";
+    cout << "Laboratory work #9. GIT\n";
+    cout << "Variant #9. Phone_Talk\n";
+    cout << "Author: Arina Paniznik\n";
     return 0;
 }
