861fe8e6cad637d97a82404cdcd1537c8df65eeb
unknown <luniakey@gmail.com>
2022-12-29 06:03:32 +0300

    Функция реализована

---
+++ unit-test-project/unit-test-project.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../main-project/phone_talk.h"
+#include "../main-project/processing.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace unittestproject
+{
+	TEST_CLASS(unittestproject)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Assert::Fail();
+		}
+	};
+}
