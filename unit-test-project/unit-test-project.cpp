4242b27f4aaf053726a641c1ddd1f9ad3e5ee5c5
ElfikJanchik <121351484+ElfikJanchik@users.noreply.github.com>
2022-12-29 13:31:29 +0300

    Update unit-test-project.cpp

--- unit-test-project/unit-test-project.cpp
+++ unit-test-project/unit-test-project.cpp
@@ -31,7 +31,7 @@ namespace unittestproject
 			subscriptions[0] = build_subscription(0, 23, 13, 0.65);
 			subscriptions[1] = build_subscription(0, 0, 12, 0.03);
 			subscriptions[2] = build_subscription(0, 0, 3, 0.01);
-			Assert::AreEqual(0.000490057, process(subscriptions, 3), 1);
+			Assert::AreEqual(0.000490057, process(subscriptions, 3));
 			delete_subscription(subscriptions, 3);
 
 		}
@@ -41,7 +41,7 @@ namespace unittestproject
 			subscriptions[0] = build_subscription(0, 46, 21, 0.97);
 			subscriptions[1] = build_subscription(0, 34, 1, 0.78);
 			subscriptions[2] = build_subscription(0, 23, 3, 0.61);
-			Assert::AreEqual(0.000380338, process(subscriptions, 3), 1);
+			Assert::AreEqual(0.000380338, process(subscriptions, 3));
 			delete_subscription(subscriptions, 3);
 
 		}
@@ -51,7 +51,7 @@ namespace unittestproject
 			subscriptions[0] = build_subscription(1, 1, 2, 1.31);
 			subscriptions[1] = build_subscription(0, 12, 11, 5.03);
 			subscriptions[2] = build_subscription(0, 49, 35, 0.99);
-			Assert::AreEqual(0.000994843, process(subscriptions, 3), 1);
+			Assert::AreEqual(0.000994843, process(subscriptions, 3));
 			delete_subscription(subscriptions, 3);
 
 		}
